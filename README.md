dd2ac6598e48ad45df89dcedd0cc14622ad4fb36
Rohit Jain <www.rohitjain.com@gmail.com>
2023-02-04 10:05:00 +0000

    AI MODEL

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# image-detector
+AI Model for Checking Numbers in Images
+This AI model is designed to recognize and classify numbers in images. The model uses deep learning techniques to perform image classification.
+
+Requirements
+Python 3.x
+TensorFlow 2.x
+OpenCV
+Matplotlib
+Numpy
+Installation
+Clone this repository to your local machine using the following command:
+bash
+Copy code
+git clone https://github.com/<username>/ai-number-recognition.git
+Change into the directory of the cloned repository:
+bash
+Copy code
+cd ai-number-recognition
+Install the required packages using the following command:
+Copy code
+pip install -r requirements.txt
+Usage
+Run the following command to train the model:
+Copy code
+python train.py
+Run the following command to test the model on a sample image:
+css
+Copy code
+python predict.py --image <path_to_image>
+Contributing
+Contributions are what make the open-source community such an amazing place to be. Any contributions you make are greatly appreciated.
+
+Fork the project
+Create your feature branch (git checkout -b feature/AmazingFeature)
+Commit your changes (git commit -m 'Add some AmazingFeature')
+Push to the branch (git push origin feature/AmazingFeature)
+Open a pull request
---
+++ train.py
@@ -0,0 +1,52 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+
+# Load the MNIST dataset
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Preprocess the data
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+# Reshape the data for the model
+img_rows, img_cols = 28, 28
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+# One-hot encode the target labels
+num_classes = 10
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+# Define the model architecture
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+# Compile the model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Train the model
+model.fit(x_train, y_train, epochs=10, batch_size=128, verbose=1, validation_data=(x_test, y_test))
+
+# Evaluate the model
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
